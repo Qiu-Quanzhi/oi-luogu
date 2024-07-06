b09ea670400c361f098fc5eaa5f6d8a1aa87b4b3
Qiu-Quanzhi <i@qqzhi.cc>
2024-07-06 21:58:38 +0800

    sync

--- contest/20240706/F.cpp
+++ contest/20240706/F.cpp
@@ -1,4 +1,60 @@
 #include <cstdio>
+#include <set>
+using namespace std;
+struct node{
+    int ch[200],n;
+    bool s,ss;
+};
+node a[300001];
+int n,k;
+set<int> ans;
+int abs(int x){
+    if (x<0)return -x;
+    return x;
+}
+void input(){
+    scanf("%d%d",&n,&k);
+    for (int i=1,tmp;i<=n;i++){
+        scanf("%d",&tmp);
+        a[tmp].ch[a[tmp].n++]=i;
+    }
+}
+bool check(set<int>& t){
+    int l=*t.begin();auto i=t.begin();i++;
+    for (;i!=t.end();l=*(i++))
+        if (abs(*i-l)<=k)
+            return true;
+     return false;
+}
+set<int> dfs1(int x){
+    set<int> b;b.insert(x);
+    if (!a[x].n)return b;
+    for (int i=0,cnt=0;i<a[x].n;i++){
+        auto c=dfs1(a[x].ch[i]);
+        if (a[a[x].ch[i]].s)cnt++;
+        if(cnt>1)a[x].ss=true;
+        b.insert(c.begin(),c.end());
+    }
+    if (check(b))a[x].s=true;
+    return b;
+}
+void dfs2(int x){
+    if (!a[x].s)return;
+    ans.insert(x);
+    if (a[x].ss)return;
+    for (int i=0;i<a[x].n;i++)
+        dfs2(a[x].ch[i]);
+}
+void solve(){
+    dfs1(a[0].ch[0]);
+    // for (int i=0;i<=n;i++){
+    //     printf ("%d ",a[i].s);
+    // }
+    dfs2(a[0].ch[0]);
+    for(auto i=ans.begin();i!=ans.end();i++)
+        printf("%d ",*i);
+}
 int main(){
-    
+    input();
+    solve();
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,48 @@
-#include <iostream>
+#include <cstdio>
+#include <set>
 using namespace std;
+struct node{
+    int fa;
+    int ch[100000],n;
+    bool s;
+};
+node a[300001];
+int n,k;
+int abs(int x){
+    if (x<0)return -x;
+    return x;
+}
+void input(){
+    scanf("%d%d",&n,&k);
+    for (int i=1,tmp;i<=n;i++){
+        scanf("%d",&tmp);
+        a[tmp].ch[a[tmp].n++]=i;
+    }
+}
+bool check(set<int>& t){
+    int l=*t.begin();auto i=t.begin();i++;
+    for (;i!=t.end();l=*(i++))
+        if (abs(*i-l)<=k)
+            return true;
+     return false;
+}
+set<int> dfs(int x){
+    set<int> b;b.insert(x);
+    if (!a[x].n)return b;
+    for (int i=0;i<a[x].n;i++){
+        auto c=dfs(a[x].ch[i]);
+        b.insert(c.begin(),c.end());
+    }
+    if (check(b))a[x].s=true;
+    return b;
+}
+void solve(){
+    dfs(0);
+    for (int i=0;i<=n;i++){
+        printf ("%d ",a[i].s);
+    }
+}
 int main(){
-    cout<<-5/3;
+    input();
+    //solve();
 }
